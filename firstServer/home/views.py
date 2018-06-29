53883866eaa0f8c7fefbfd782d1b84c0cae47749
my4831218 <my2853121@naver.com>
2018-06-29 16:42:31 +0900

    중간수정 + 버튼선택에 대한 답변

--- firstServer/home/views.py
+++ firstServer/home/views.py
@@ -3,10 +3,22 @@ from django.http import HttpResponse
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
-def keyboard(request):
-    return JsonResponse (
+def keyboard(request) :
+    return JsonResponse(
     {
-    "type" : "buttons",
-    "buttons" : ["선택 1", "선택 2", "선택 3"],
+        "type" : "buttons",
+        "buttons" : ["선택 1", "선택 2", "선택 3"],
     }
-    )
+)
+def message(request) :
+    message = ((request.body).decode('utf-8'))
+    return_json_str = json.loads(message)
+    return_str = return_json_str['content']
+    if return_str == 선택1:
+        return JsonResponse(
+            { #return 밑에는 공통어
+            "message": {
+                "text": "안녕하세요?"
+            }
+        }
+        )
