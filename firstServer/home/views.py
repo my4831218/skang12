4089b37c75dceb8b32c2b4711847a961ac8b8aa3
my4831218 <my2853121@naver.com>
2018-06-29 16:53:43 +0900

    문법수정

--- firstServer/home/views.py
+++ firstServer/home/views.py
@@ -15,12 +15,13 @@ def keyboard(request) :
         "buttons" : ["선택 1", "선택 2", "선택 3"],
     }
 )
+
 @csrf_exempt
 def message(request) :
     message = ((request.body).decode('utf-8'))
     return_json_str = json.loads(message)
     return_str = return_json_str['content']
-    if return_str == 선택1:
+    if return_str == 선택 1:
         return JsonResponse(
             { #return 밑에는 공통어
                 "message": {
