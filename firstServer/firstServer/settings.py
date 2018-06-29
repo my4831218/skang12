faebc4c5464b0410c437bd745e322dde71f0ecd1
my4831218 <my2853121@naver.com>
2018-06-29 15:34:26 +0900

    모든 ip허용 시간대 서울로바꿈

--- firstServer/firstServer/settings.py
+++ firstServer/firstServer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^s_8!j*@o8p_+_-r-v39va39+!+e^xokqr05+(^dxr!m#ezs4i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = '*'
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
